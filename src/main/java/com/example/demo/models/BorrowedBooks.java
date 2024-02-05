9cb2a1760fbebdcc7a4ca0840f006235930e83ab
Oskar Gester <gester.oskar@gmail.com>
2024-02-05 14:32:48 +0100

    renamed project

--- src/main/java/com/example/demo/models/BorrowedBooks.java
+++ src/main/java/com/example/demo/models/BorrowedBooks.java
@@ -1,10 +1,61 @@
 package com.example.demo.models;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 @Document(collection = "borrowedBooks")
 public class BorrowedBooks {
     @Id
     private String id;
+    //@DocumentReference(lazy = true) private List<Book> books;
 
+    //@DocumentReference(collection = "books")
+    private String[] borrowedBooks;
+    //@DocumentReference
+    //private List<Book> books;
+
+    //@DocumentReference(collection = "users")
+    private String userId;
+
+    @CreatedDate
+    private Date borrowedAt;
+
+
+    private LocalDate returnAt = LocalDate.now().plusDays(14);
+
+
+    public BorrowedBooks() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String[] getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public Date getBorrowedAt() {
+        return borrowedAt;
+    }
+
+    public LocalDate getReturnAt() {
+        return returnAt;
+    }
+
+    public void setReturnAt(LocalDate returnAt) {
+        this.returnAt = returnAt;
+    }
+
+   /* public List<Book> getBooks() {
+        return books;
+    }*/
 }
--- src/main/java/com/example/demo/repository/BorrowedBooksRepository.java
+++ src/main/java/com/example/demo/repository/BorrowedBooksRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface BorrowedBooksRepository {
+import com.example.demo.models.BorrowedBooks;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BorrowedBooksRepository extends MongoRepository<BorrowedBooks, String> {
 }
